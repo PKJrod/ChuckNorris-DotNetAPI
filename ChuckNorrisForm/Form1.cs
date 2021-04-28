2efd971544a362ad547af3525ed00bf91933626f
Justin Rodriguez <justinrodriguez@live.com>
2021-04-27 19:21:56 -0700

    Refeerenced to ChuckNorrisAPI project

--- ChuckNorrisForm/Form1.cs
+++ ChuckNorrisForm/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ChuckNorrisAPI;
 
 namespace ChuckNorrisForm
 {
@@ -15,6 +16,16 @@ namespace ChuckNorrisForm
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void generateJokeCmd_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
